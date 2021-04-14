0d09ebf78a8d7761ffb942899bc63b6e48355e09
Wesley <wes.beck21@outlook.com>
2021-04-14 15:21:15 -0700

    header styling done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Home from './Home.js'
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build the amazon store</h1>
       <Header />
-      {/* HOME */}
+      <Home />
     </div>
   );
 }
