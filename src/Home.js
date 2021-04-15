ef02ff612ad35c0365670ad8e959243779ce396b
Wesley <wes.beck21@outlook.com>
2021-04-14 17:15:01 -0700

    demo product w/props

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,12 @@ function Home() {
       <div className="home__container">
         <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/digital/adrive/photos/gateway/APH_feb-21_hero_1x._CB661372629_.jpg" alt="" />
         <div className="home__row">
-          <Product />
+          <Product  
+          title='The Lean Startup'
+          price={29.99}
+          image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg'
+          rating={5}
+          />
           <Product />
         </div>
         <div className="home__row">
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({title, image, price, rating}) {
   return (
     <div className="product">
         <div className="product__info">
-          <p>The lean startup</p>
+          <p>{title}</p>
           <p className="product__price">
             <small>$</small>
-            <strong>19.99</strong>
+            <strong>{price}</strong>
           </p>
           <div className="product__rating">
-            <p>⭐</p>
+            {Array(rating).fill().map((_, i) => (
+              <p>⭐</p>
+            ))}
           </div>
         </div>
 
-        <img src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg" alt=""/>
+        <img src={image} alt=""/>
         
         <button> Add to Basket </button>
 
