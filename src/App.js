8696c76e8583011814fc2b2acc3672fe86f2a6b0
Wesley <wes.beck21@outlook.com>
2021-04-14 18:36:10 -0700

    unfinished checkout code

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 import Header from './Header'
 import Home from './Home';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Checkout from './Checkout';
 
 
 function App() {
   return (
     <Router>
     <div className="app">
+      <Header />
       <Switch>
+        
         <Route exact path="/checkout">
           <Checkout />
         </Route>
         <Route path="/">
-          <Header />
           <Home />
         </Route>
-        
+
       </Switch>
     </div>
     </Router>
--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,20 @@ import './Checkout.css'
 function Checkout() {
   return (
     <div className="checkout">
-      <h1>checkout page</h1>
+      <div className="checkout__left">
+        <img 
+          className="checkout__ad" 
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" 
+          />
+
+          <div>
+            <h2 className="checkout__title">Your shopping Basket</h2>
+          </div>
+
+          <div className="checkout__right">
+            <h2>subtotal will go here</h2>
+          </div>
+      </div>
     </div>
   )
 }
