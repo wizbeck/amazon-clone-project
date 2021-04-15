4f3cac32bd344b6587cc82bec80ae27d6ebd08ab
Wesley <wes.beck21@outlook.com>
2021-04-14 19:20:52 -0700

    header links

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
+      <Link to="/">
       <img className="header__logo"src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+      </Link>
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon"/>
@@ -25,10 +28,12 @@ function Header() {
           <span className="header__optionLineOne"> Your</span>
           <span className="header__optionLineTwo"> Prime</span>
         </div>
+        <Link to="/checkout">
         <div className="header__optionBasket">
           <ShoppingBasketIcon />
           <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
+        </Link>
       </div>
     </div>
   )
