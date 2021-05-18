6b57c7bde7e02c90271c5b1fff2d10acf5169a26
Wesley <wes.beck21@outlook.com>
2021-05-17 22:40:58 -0700

    remove console log, add sign in sign out functions

--- src/Product.js
+++ src/Product.js
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider'
 function Product({id, title, image, price, rating}) {
 
   const [{basket}, dispatch] = useStateValue();
-  console.log('this is the basket >>>', basket)
+  
   const addToBasket = () => {
     //dispatch item into store(datalayer)
     dispatch({
